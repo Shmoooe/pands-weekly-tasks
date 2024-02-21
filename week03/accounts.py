218cc740bfcc78078cdd19be1e0c3c8ba0d63658
Joanna Kelly <joannabkelly@gmail.com>
2024-02-21 21:20:36 +0000

    Updated a little bit to the week02 task

--- week03/accounts.py
+++ week03/accounts.py
@@ -4,12 +4,12 @@
 # Sliced the string and used the formatting function to "replace" the first 6 digits with Xs.
 # I'm certain this is not the best way to approach this and will have to return in future.
 # Consulted W3Schools, tried to use the replace() function as replace([:6], XXXXXX) but that did not work.
-# As it stands now, this program does not account for 10-digit numbers.
+# This program can now account for numbers longer than 10-digits, but returns the same number of Xs as a 10-digit number.
 
 
 inputAccountNumber = input("Please enter a 10 digit account number: ")
 
-lastDigits = inputAccountNumber[6:]
+lastDigits = inputAccountNumber[-4:]
 
 
 print(f"XXXXXX{lastDigits}")
